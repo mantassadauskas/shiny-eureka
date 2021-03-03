cf3dd95f20352d056c21e3841506850eafdb944e
msadauskas <xdmin000@gmail.com>
2021-03-03 16:50:03 +0200

    fix old password selector

--- src/utils/oldPassword.ts
+++ src/utils/oldPassword.ts
@@ -1,17 +1,12 @@
 import { IItem } from '~/services/getUserItems';
-import { TimeInMilliseconds } from '~/constants';
+import { TimeInMilliseconds } from '../constants';
 
-export const itemHasOldPassword = (item: IItem, itemList: Array<IItem>) => {
-  const oldItems = itemList.filter((listItem) => {
-      const passwordDateInMilliseconds = new Date(listItem.createdAt).getTime();
-      return Date.now() - passwordDateInMilliseconds > TimeInMilliseconds.Days30
-    }
-  )
-
-  return oldItems.length;
+export const itemHasOldPassword = (item: IItem) => {
+  const passwordDateInMilliseconds = new Date(item.createdAt).getTime();
+  return Date.now() - passwordDateInMilliseconds > TimeInMilliseconds.Days30
 };
 
 export const getOldPasswordsCount = (items: IItem[]) => items.reduce((count, item) => (
-  itemHasOldPassword(item, items) ? (count + 1) : count
+  itemHasOldPassword(item) ? (count + 1) : count
 ), 0)
 
