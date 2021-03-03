4106ca862691d943eb8a229079f264bda3b038da
msadauskas <xdmin000@gmail.com>
2021-03-03 16:50:29 +0200

    extract fixture from test. Create test for old password

---
+++ src/fixtures.ts
@@ -0,0 +1,23 @@
+export const items = [
+  {
+    "id": "000",
+    "title": "discord",
+    "description": "rumors",
+    "password": "discordPassword123.",
+    "createdAt": new Date().toISOString()
+  },
+  {
+    "id": "001",
+    "title": "airdroid",
+    "description": "replace android",
+    "password": "pass1",
+    "createdAt": new Date(new Date().setMonth(new Date().getMonth() - 2)).toISOString()
+},
+  {
+    "id": "010",
+    "title": "Nintendo",
+    "description": "Lets play",
+    "password": "pass1",
+    "createdAt": new Date().toISOString()
+  }
+]
