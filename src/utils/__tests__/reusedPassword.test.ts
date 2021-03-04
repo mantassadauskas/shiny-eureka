1417a7b52498ea39274fc0ce35e3972534d7e36d
msadauskas <xdmin000@gmail.com>
2021-03-04 16:17:22 +0200

    change test item

--- src/utils/__tests__/reusedPassword.test.ts
+++ src/utils/__tests__/reusedPassword.test.ts
@@ -3,5 +3,5 @@ import { items } from '../../fixtures';
 
 test('should return true if there is more than one item with same password', () => {
    expect(itemHasReusedPassword(items[0], items)).toBe(false);
-   expect(itemHasReusedPassword(items[2], items)).toBe(true);
+   expect(itemHasReusedPassword(items[3], items)).toBe(true);
 });
