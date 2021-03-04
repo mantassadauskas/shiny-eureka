f16dbd31c9bf9956d0a69ac7667f388859d863c4
msadauskas <xdmin000@gmail.com>
2021-03-04 09:34:29 +0200

    remove empty spaces

--- src/utils/__tests__/oldPassword.test.ts
+++ src/utils/__tests__/oldPassword.test.ts
@@ -1,8 +1,6 @@
 import { itemHasOldPassword } from '../oldPassword';
 import { items } from '../../fixtures';
 
-
-
 test('should return true if password is older than 30 days', () => {
   expect(itemHasOldPassword(items[0])).toBe(false);
   expect(itemHasOldPassword(items[1])).toBe(true);
--- src/utils/__tests__/reusedPassword.test.ts
+++ src/utils/__tests__/reusedPassword.test.ts
@@ -1,7 +1,6 @@
 import { itemHasReusedPassword } from '../reusedPassword';
 import { items } from '../../fixtures';
 
-
 test('should return true if there is more than one item with same password', () => {
    expect(itemHasReusedPassword(items[0], items)).toBe(false);
    expect(itemHasReusedPassword(items[2], items)).toBe(true);
